7a3c4b484ef4e20da20fc6f9eab4c2bae7bfa0b3
darojasc123 <da.rojasc123@uniandes.edu.co>
2024-04-28 14:21:17 -0500

    Cambios Modelo Luis David

---
+++ Semana 3/model/m09_model_deployment_LD.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+
+
+def predict_price(year,mileage,state,make,model):
+
+    Rgr = joblib.load(os.path.dirname(__file__) + '/CarPriceRegressor2.pkl') 
+
+    # Create features
+    
+    make_code = pd.read_csv('df_make_code.csv')
+    make_dict = make_code.set_index('Make')['Code'].to_dict()
+
+    model_code = pd.read_csv('df_model_code.csv')
+    model_dict = model_code.set_index('Model')['Code'].to_dict()
+
+    state_code = pd.read_csv('df_state_code.csv')
+    state_dict = state_code.set_index('State')['Code'].to_dict()
+    
+
+    var_year = int(year)
+    var_mileage = float(mileage)
+    var_state = int(state_dict[state.upper()])
+    var_make = int(make_dict[make.upper()])
+    var_model = int(model_dict[model.upper()])
+    #media = 55072.956895
+    #desviacion = 40880.96774371
+    
+    
+    #df['Mileage'] = (var_mileage-media)/desviacion
+
+    df = pd.DataFrame(columns=['Make', 'Mileage', 'Model','State','Year'])
+    
+    df['Make'] = var_make
+    df['Mileage'] = var_mileage
+    df['Model'] = var_model
+    df['State'] = var_state
+    df['Year'] = var_year
+
+    
+    df = df.sort_index(axis=1)
+
+
+    # Make prediction
+    p1 = Rgr.predict(df)
+
+    return p1
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add an URL')
+        
+    else:
+
+        year = sys.argv[1]
+        mileage = sys.argv[2]
+        state = sys.argv[3]
+        make = sys.argv[4]
+        model = sys.argv[5]
+
+        p1 = predict_price(year,mileage,state,make,model)
+        
+        print('Year: ' + str(year) + ' ' + 'Mileage: ' + str(mileage) + ' '  + 'State: ' + str(state) + ' ' + 'Make: ' + str(make) + ' ' +'Model: ' + str(model))
+        print("Car's Price: ", p1)
+        
