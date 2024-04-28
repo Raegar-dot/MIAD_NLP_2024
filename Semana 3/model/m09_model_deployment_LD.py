c5301d903b4b5628453427968261c83dcde77c7d
darojasc123 <da.rojasc123@uniandes.edu.co>
2024-04-28 18:21:14 -0500

    Ajuste servicio LD

--- Semana 3/model/m09_model_deployment_LD.py
+++ Semana 3/model/m09_model_deployment_LD.py
@@ -28,22 +28,29 @@ def predict_price(year,mileage,state,make,model):
     var_state = int(state_dict[' '+ state.upper()])
     var_make = int(make_dict[make.upper()])
     var_model = int(model_dict[model.upper()])
+    print(var_year)
+    print(var_mileage)
+    print(var_state)
+    print(var_make)
+    print(var_model)
     #media = 55072.956895
     #desviacion = 40880.96774371
     
     
     #df['Mileage'] = (var_mileage-media)/desviacion
 
-    df = pd.DataFrame(columns=['Year', 'Mileage','State','Make', 'Model'])
+    #df = pd.DataFrame(columns=['Year', 'Mileage','State','Make', 'Model'])
     
-    df['Year'] = var_year
-    df['Mileage'] = var_mileage
-    df['State'] = var_state
-    df['Make'] = var_make
-    df['Model'] = var_model
-    
-    
+    data = {
+        'Year': [var_year],
+        'Mileage': [var_mileage],
+        'State': [var_state],
+        'Make': [var_make],
+        'Model': [var_model]    
+        }
+    df = pd.DataFrame(data)
 
+    print(df)
 
 
     # Make prediction
